38e878546eea7c0d8e2232ec978855746b958267
Ian Schlarman <ischlarman@gmail.com>
2021-02-02 17:49:33 -0600

    Added logging support.

---
+++ src/Extensions.Services.Hosting/ServiceExtensions.Hosting/LoggerAccess.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceExtensions.Hosting;
+using System;
+
+namespace Microsoft.Extensions.Logging
+{
+    /// <summary>
+    /// Methods exposing global access to logging facilities.
+    /// </summary>
+    public static class LoggerAccess
+    {
+        private static ILoggerFactory LogFactory
+            => HostAccess.ServiceProvider.GetRequiredService<ILoggerFactory>();
+
+        /// <inheritdoc cref="LoggerFactoryExtensions.CreateLogger{T}(ILoggerFactory)" />
+        public static ILogger<TCategory> GetLogger<TCategory>()
+            => LogFactory.CreateLogger<TCategory>();
+
+        /// <inheritdoc cref="LoggerFactoryExtensions.CreateLogger(ILoggerFactory, Type)" />
+        public static ILogger GetLogger(Type consumer)
+            => LogFactory.CreateLogger(consumer);
+    }
+}
